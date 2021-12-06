599e135da7f56ba679afb2cf414a4bd45d1bf8cb
matthewhale <matthewhale207@gmail.com>
2021-12-06 08:24:28 -0500

    Day 6 + 6.5 complete

---
+++ 6.5/main.py
@@ -0,0 +1,25 @@
+#AoC Day 6, Part 2
+
+import copy
+import numpy as np
+
+fish = np.array([1,1,1,1,1,5,1,1,1,5,1,1,3,1,5,1,4,1,5,1,2,5,1,1,1,1,3,1,4,5,1,1,2,1,1,1,2,4,3,2,1,1,2,1,5,4,4,1,4,1,1,1,4,1,3,1,1,1,2,1,1,1,1,1,1,1,5,4,4,2,4,5,2,1,5,3,1,3,3,1,1,5,4,1,1,3,5,1,1,1,4,4,2,4,1,1,4,1,1,2,1,1,1,2,1,5,2,5,1,1,1,4,1,2,1,1,1,2,2,1,3,1,4,4,1,1,3,1,4,1,1,1,2,5,5,1,4,1,4,4,1,4,1,2,4,1,1,4,1,3,4,4,1,1,5,3,1,1,5,1,3,4,2,1,3,1,3,1,1,1,1,1,1,1,1,1,4,5,1,1,1,1,3,1,1,5,1,1,4,1,1,3,1,1,5,2,1,4,4,1,4,1,2,1,1,1,1,2,1,4,1,1,2,5,1,4,4,1,1,1,4,1,1,1,5,3,1,4,1,4,1,1,3,5,3,5,5,5,1,5,1,1,1,1,1,1,1,1,2,3,3,3,3,4,2,1,1,4,5,3,1,1,5,5,1,1,2,1,4,1,3,5,1,1,1,5,2,2,1,4,2,1,1,4,1,3,1,1,1,3,1,5,1,5,1,1,4,1,2,1], dtype=np.int8)
+#length = len(lanternfish)
+tempfish = [0,0,0,0,0,0,0,0,0]
+for individualfish in range(0,300):
+    tempfish[fish[individualfish]] += 1
+print(tempfish)
+
+for i in range(1,257):
+    print("Day ", i)
+    zerofish = copy.deepcopy(tempfish[0])
+    for i in range(0,8):
+        print(i)
+        print(tempfish)
+        tempfish[i] = copy.deepcopy(tempfish[i+1])
+    tempfish[8] = zerofish
+    tempfish[6] += zerofish
+finalfish = 0
+for i in range(0,9):
+    finalfish += tempfish[i]
+print("totalfish: ", finalfish)
