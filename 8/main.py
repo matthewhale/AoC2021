8eb469f092b9a1673ef5d2ad79f831dc8edd87c3
matthewhale <matthewhale207@gmail.com>
2021-12-08 06:40:51 -0500

    Day 8 complete

---
+++ 8/main.py
@@ -0,0 +1,12 @@
+import copy
+import csv
+import numpy
+
+counts = 0
+with open('input.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for line in csvreader:
+        for i in range(10,14):
+            if (len(line[i]) == 2) or (len(line[i]) == 3) or (len(line[i]) == 4) or (len(line[i]) == 7):
+                counts += 1
+print("Number of 1, 4, 7, 8's: ", counts)
